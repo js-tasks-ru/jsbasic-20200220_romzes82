5277b86fb79dd41fac10f43684e5d0a2373878c6
Roman Maslennikov <romzes120282@gmail.com>
2020-03-02 11:59:32 +0300

    Функция сложения двух чисел

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,11 +1,6 @@
-/**
- * Power
- * @param {number} m base
- * @param {number} n index
- * @returns {number}
- */
 function sum(m, n) {
-  return (m+n);
+  let result = m + n;
+  return (m + n);
 }
 
- //console.log(sum(6,3));
+// console.log(sum(5,4));
