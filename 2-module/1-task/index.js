74d22c8717d48e75417accd76f1f971bc9991ad3
Roman Maslennikov <romzes120282@gmail.com>
2020-03-04 18:01:50 +0300

    Функция для суммирования всех зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,14 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
 
-}
+function sumSalary(salaries) {
+  let sum = 0;
+    for (const key in salaries) {
+      //console.log(typeof(obj[key]));
+      if (typeof(salaries[key]) === `number`) {
+        sum = sum + salaries[key];
+      }
+    }
+  return sum;
+} 
