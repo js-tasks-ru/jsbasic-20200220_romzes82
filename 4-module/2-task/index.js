40467a73139097c32bd674d5b1422e2a5bbbb430
Roman Maslennikov <romzes120282@gmail.com>
2020-03-23 13:59:00 +0300

    Функция makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,14 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+
+  let arrRows = table.rows;
+
+  for (let i = 0; i < arrRows.length; i++) {
+    //console.log(table.rows[i].cells[i].innerHTML);
+    table.rows[i].cells[i].innerHTML = table.rows[i].cells[i].innerHTML;
+    table.rows[i].cells[i].style.backgroundColor = `red`;
+  }
+
+  return table.replaceWith(table);
 }
